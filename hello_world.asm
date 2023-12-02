36f35da0eafc304c12cbf96772b78e4820bb1fb2
Tom Krush <tom@tomkrush.com>
2023-12-01 22:11:16 -0600

    Added fasm and rewrote hello world

--- hello_world.asm
+++ hello_world.asm
@@ -1,16 +1,21 @@
-section .data
-    hello_msg db 'Hello, World!',0
+; ----------------------------------------------------------------------------------------
+; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
+; To assemble and run:
+;
+;     nasm -felf64 hello.asm && ld hello.o && ./a.out
+; ----------------------------------------------------------------------------------------
 
-section .text
-    global _start
+          global    _start
 
-_start:
-    mov rax, 1              ; system call number for sys_write
-    mov rdi, 1              ; file descriptor 1 is stdout
-    mov rsi, hello_msg      ; address of string to output
-    mov rdx, 13             ; number of bytes
-    syscall                 ; invoke operating system to do the write
+          section   .text
+_start:   mov       rax, 1                  ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 13                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 60                 ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
 
-    mov rax, 60             ; system call number for sys_exit
-    xor rdi, rdi            ; exit code 0
-    syscall                 ; invoke operating system to exit
+          section   .data
+message:  db        "Hello, World", 10      ; note the newline at the end
